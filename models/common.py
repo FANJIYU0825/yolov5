8e24eb31ba8d5ca7b39e79e3e9b6cc5d66780866
FANFNA <55987247+FANJIYU0825@users.noreply.github.com>
2022-08-01 23:39:24 +0800

    Update common.py

--- models/common.py
+++ models/common.py
@@ -706,7 +706,7 @@ class Detections:
     def show(self, labels=True):
         self.display(show=True, labels=labels)  # show results
 
-    def save(self, labels=True, save_dir='runs/detect/exp'):
+    def save(self, labels=True, save_dir='static/img/dect'):
         save_dir = increment_path(save_dir, exist_ok=save_dir != 'runs/detect/exp', mkdir=True)  # increment save_dir
         self.display(save=True, labels=labels, save_dir=save_dir)  # save results
 
