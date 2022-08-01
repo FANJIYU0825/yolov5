09835eaced105766149c4f2b60ed66f7ed9d69f0
FANFNA <55987247+FANJIYU0825@users.noreply.github.com>
2022-08-02 00:09:54 +0800

    Update common.py

--- models/common.py
+++ models/common.py
@@ -707,7 +707,7 @@ class Detections:
         self.display(show=True, labels=labels)  # show results
 
     def save(self, labels=True, save_dir='static/img'):
-        save_dir = increment_path(save_dir, exist_ok=save_dir != 'runs/detect/exp', mkdir=True)  # increment save_dir
+#         save_dir = increment_path(save_dir, exist_ok=save_dir != 'static/img', mkdir=False)  # increment save_dir
         self.display(save=True, labels=labels, save_dir=save_dir)  # save results
 
     def crop(self, save=True, save_dir='runs/detect/exp'):
